2de776c9649739c345d4160836f7785e13772dc3
chauchakching <chauchakching@gmail.com>
2021-04-25 21:08:53 +0800

    test: fix test

--- src/bisect.test.ts
+++ src/bisect.test.ts
@@ -2,24 +2,24 @@ import {bisect} from './bisect';
 
 describe('bisect', () => {
   it('should find the smallest interger that the square is over 300', async () => {
-    await expect(
-      bisect({
-        start: 1,
-        end: 100,
-        check: (val: number) => {
-          return val ** 2 <= 300;
-        },
-      })
-    ).resolves.toBe(18);
+    const [result] = await bisect({
+      start: 1,
+      end: 100,
+      check: (val: number) => {
+        return val ** 2 <= 300;
+      },
+    })
+
+    expect(result).toBe(18);
   });
 
   it('handle command', async () => {
-    await expect(
-      bisect({
-        start: 1,
-        end: 100,
-        commandToCheck: 'node test/is-square-less-than300.js %',
-      })
-    ).resolves.toBe(18);
+    const [result] = await bisect({
+      start: 1,
+      end: 100,
+      commandToCheck: 'node test/is-square-less-than300.js %',
+    })
+
+    expect(result).toBe(18);
   });
 });
